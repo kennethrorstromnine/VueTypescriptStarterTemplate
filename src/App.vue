fe7b095d6f3b4e750db4c732288ea9693a491780
Mick Neupart <min@nine.dk>
2018-09-18 16:07:51 +0200

    added Home link

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
         <b-navbar-nav>
 
           <b-nav-item>
+            <router-link to="/" class="nav-link">Home</router-link>
+          </b-nav-item>
+
+          <b-nav-item>
             <router-link to="/about" class="nav-link">About</router-link>
           </b-nav-item>
 
